caf4c815be2d16a16e7c4ac9d4272c7437e83c84
David del Río Medina <ddrm86@gmail.com>
2017-09-27 11:44:50 +0200

    Use deep tl detector to detect lights in camera image

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -11,6 +11,7 @@ import tf
 import cv2
 import yaml
 import math
+import deep_tl_detector as dtl
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -130,41 +131,9 @@ class TLDetector(object):
         return distances.index(min(distances))
 
 
-    def project_to_image_plane(self, point_in_world):
-        """Project point from 3D world coordinates to 2D camera image location
-
-        Args:
-            point_in_world (Point): 3D location of a point in the world
-
-        Returns:
-            x (int): x coordinate of target point in image
-            y (int): y coordinate of target point in image
-
-        """
-
-        fx = self.config['camera_info']['focal_length_x']
-        fy = self.config['camera_info']['focal_length_y']
-        image_width = self.config['camera_info']['image_width']
-        image_height = self.config['camera_info']['image_height']
-
-        # get transform between pose of camera and world frame
-        trans = None
-        try:
-            now = rospy.Time.now()
-            self.listener.waitForTransform("/base_link",
-                  "/world", now, rospy.Duration(1.0))
-            (trans, rot) = self.listener.lookupTransform("/base_link",
-                  "/world", now)
-
-        except (tf.Exception, tf.LookupException, tf.ConnectivityException):
-            rospy.logerr("Failed to find camera to map transform")
-
-        #TODO Use tranform and rotation to calculate 2D position of light in image
-
-        x = 0
-        y = 0
-
-        return (x, y)
+    def get_light_box(self, image):
+        light_box = dtl.detect_light(image)
+        return light_box
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -182,12 +151,15 @@ class TLDetector(object):
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        x, y = self.project_to_image_plane(light.pose.pose.position)
+        box = self.get_light_box(cv_image)
+        if box is not None:
+            left, right, top, bottom = self.get_light_box(cv_image)
 
-        #TODO use light location to zoom in on traffic light in image
-
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)
+            crop_img = cv_image[int(round(top)):int(round(bottom)), 
+                                int(round(left)):int(round(right))]
+            rospy.loginfo('Traffic light found at %f, %f, %f, %f', left, right, top, bottom)
+            return self.light_classifier.get_classification(crop_img)
+        return TrafficLight.UNKNOWN
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
